673e1b136b9377613f9b909e2a0ff22038fd8334
Fabian Enrique Suarez Carvajal <fsuarez120@unab.edu.co>
2026-02-12 10:53:22 -0500

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/mi-nombre', function () {
+    return "FABIAN ENRIQUE SUAREZ";
+});
+
+Route::get('/php-basico...', function () {
+    
+
+    return "FABIAN ENRIQUE SUAREZ";
+});
